a45247949f62b7c9f71b150e42f2b14553069d84
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-08 04:34:45 +0330

    Add server 2 and mainjs file to sett a get request on another server and provide a file to download

---
+++ server2/main.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/download", (req, res) => {
+  res.download("./downloadfilename");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
