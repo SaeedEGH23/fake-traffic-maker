6ea034f1f82e22384a57883712283cf918b207b3
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-08 05:13:25 +0330

    Correcting the filename in get request

--- server2/main.js
+++ server2/main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/download", (req, res) => {
-  res.download("./downloadfilename");
+  res.download("./filename");
 });
 
 app.listen(3000, () => {
