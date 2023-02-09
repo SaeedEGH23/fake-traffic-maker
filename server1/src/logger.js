d624ee65c3344c57899248a141c4cad8abcf93d2
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-10 02:05:47 +0330

    Check directory exist

--- server1/src/logger.js
+++ server1/src/logger.js
@@ -1,13 +1,17 @@
 const fs = require("fs");
-
+const { nextTick } = require("process");
+const dirPath = "./logs";
 let today, date, time, log_file;
 
 function updateTime() {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
   today = new Date();
   date =
     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  log_file = "./logs/" + "log" + date + ".txt";
+  log_file = `${dirPath}/` + "log" + date + ".txt";
 }
 
 module.exports = {
