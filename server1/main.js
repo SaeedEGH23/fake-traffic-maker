3e92916b30342478e8b87dbc6a64b6b4420e11d3
Saeed Ghorbaninezhad <saeed.ghorbaninezhad@gmail.com>
2023-02-10 01:11:06 +0330

    Import and use logger

---
+++ server1/main.js
@@ -0,0 +1,25 @@
+const cron = require("node-cron");
+const execFile = require("child_process");
+const logger = require("./src/logger");
+
+async function downloadFile() {
+  await execFile.exec("bash download.sh", (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+      logger.log(error);
+    }
+    if (stdout) {
+      console.log(stdout);
+      logger.log(stdout);
+    }
+    if (stderr) {
+      console.log(stderr);
+      logger.log(stderr);
+    }
+  });
+}
+//Run module every 10 minutes
+cron.schedule("*/15 * * * * *", () => {
+  downloadFile();
+  console.log("running a task every 10 minute");
+});
